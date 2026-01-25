5f4f7783f648ed57eee791cba0440a778285ca0b
maratmarat22 <akhmyarovmarat@mail.ru>
2026-01-25 03:29:24 +0300

    Деплой, попытка 2

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import { defineConfig } from 'vite';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/auto-mini-app-v2/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
